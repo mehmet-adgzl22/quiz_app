01a67cd93dc15a0996e04a3127d7e0bac08f49a4
mehmet.adgzl22 <mehmetadiguzel22@hotmail.com>
2025-02-06 10:50:41 +0300

    first commit

---
+++ questions.js
@@ -0,0 +1,117 @@
+let questions = [
+   
+    {
+        numb: 1,
+            question: "Hz. Hamza Hangi Savaşta Şehid Oldu?",
+            answer: "C. Uhud Savaşı",
+            options: [
+                "A. Bedir Savaşı ",
+                "B. Hendek Savaşı",
+                "C. Uhud Savaşı",
+                "D. Hayber Savaşı"
+            ]
+    },
+    {
+        numb: 2,
+            question: "Hicret sırasında Hz. Peygamber'i (s.a.v) evinde misafir eden sahabe kimdir?",
+            answer: "B. Hz. Ebu Eyyub El-Ensari (r.a)",
+            options: [
+                "A. Hz. Osman (r.a)",
+                "B. Hz. Ebu Eyyub El-Ensari (r.a)",
+                "C. Hz. Ali (r.a)",
+                "D. Hz. Bilal (r.a)"
+            ]
+    },
+    {
+        numb: 3,
+            question: "Hz. Muhammed'e (s.a.v) ilk vahiy hangi dağda inmiştir?",
+            answer: "C. Hira Dağı",
+            options: [
+                "A. Uhud Dağı",
+                "B. Sevr Dağı",
+                "C. Hira Dağı",
+                "D. Sina Dağı"
+            ]
+    },
+    {
+        numb: 4,
+           question: "İslam'da ilk halife kimdir?",
+            answer: "D. Hz. Ebu Bekir (r.a)",
+            options: [
+                "A. Hz. Ali (r.a)",
+                "B. Hz. Ömer (r.a)",
+                "C. Hz. Osman (r.a)",
+                "D. Hz. Ebu Bekir (r.a)"
+            ]
+    },
+    
+    {
+        numb: 5,
+        question: "Hz. Muhammed'in (s.a.v) en küçük kızı kimdir?",
+        answer: "B. Hz. Fatıma (r.a)",
+        options: [
+            "A. Hz. Rukiyye (r.a)",
+            "B. Hz. Fatıma (r.a)",
+            "C. Hz. Zeynep (r.a)",
+            "D. Hz. Ümmü Gülsüm (r.a)"
+        ]
+    },
+
+    {
+        numb: 6,
+            question: "Hz. Muhammed (s.a.v) kaç yaşında peygamber olmuştur?",
+            answer: "C. 40",
+            options: [
+                "A. 30",
+                "B. 35",
+                "C. 40",
+                "D. 45"
+      ]
+    },
+    
+    {
+        numb: 7,
+        question: "Hz. Muhammed'in (s.a.v) peygamber olmadan önce lakabı neydi?",
+        answer: "D. El-Emin",
+        options: [
+            "A. Es-Sıddık",
+            "B. El-Faruk",
+            "C. Zinnureyn",
+            "D. El-Emin"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Bedir Savaşı hangi yıl gerçekleşmiştir?",
+        answer: "B. 624",
+        options: [
+            "A. 622",
+            "B. 624",
+            "C. 627",
+            "D. 632"
+        ]
+    },
+    
+    {
+        numb: 9,
+        question: "İslam'da en uzun ömürlü halife kimdir?",
+        answer: "C. Hz. Osman (r.a)",
+        options: [
+            "A. Hz. Ebu Bekir (r.a)",
+            "B. Hz. Ömer (r.a)",
+            "C. Hz. Osman (r.a)",
+            "D. Hz. Ali (r.a)"
+        ]
+    },
+    {
+        numb: 10,
+        question: "İlk Müslüman çocuk kimdir?",
+        answer: "A. Hz. Ali (r.a)",
+        options: [
+            "A. Hz. Ali (r.a)",
+            "B. Hz. Hasan (r.a)",
+            "C. Hz. Hüseyin (r.a)",
+            "D. Hz. Abdullah bin Zübeyr (r.a)"
+        ]
+    },
+];
